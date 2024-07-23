dba0d1e91efbb0ee65e3af502c445eaf8158e232
NinadMaladkar <maladkar.ninad@gmail.com>
2024-07-23 23:24:04 +0200

    Fix logo

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Image, keyframes, usePrefersReducedMotion } from '@chakra-ui/react';
+import {
+  Image,
+  keyframes,
+  usePrefersReducedMotion,
+  ImageProps,
+} from '@chakra-ui/react';
 import logo from './logo.svg';
 
 const spin = keyframes`
   from { transform: rotate(0deg); }
   to { transform: rotate(360deg); }
 `;
+interface LogoProps extends ImageProps {}
 
-export const Logo = props => {
+export const Logo = (props: LogoProps) => {
   const prefersReducedMotion = usePrefersReducedMotion();
 
   const animation = prefersReducedMotion
